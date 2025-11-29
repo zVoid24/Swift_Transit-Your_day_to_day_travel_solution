351acc479954425cc5c9d5020532df18c15a0dbe
zVoid24 <rabbi1272@gmail.com>
2025-11-30 00:49:17 +0600

    ..

--- app/swifttransit/lib/core/constants.dart
+++ app/swifttransit/lib/core/constants.dart
@@ -2,5 +2,6 @@ class AppConstants {
   // Use 10.0.2.2 for Android emulator to access localhost
   // Use localhost for iOS simulator
   // Use your machine's IP for physical devices
-  static const String baseUrl = "http://10.0.2.2:8080";
+  static const String baseUrl =
+      "https://thermosetting-paralexic-paulene.ngrok-free.dev";
 }
